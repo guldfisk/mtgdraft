a82ba1d6d138e78b01a7fb6709fd9a1346f650cf
carl-emil hjort-trærup <ce.guldfisk@gmail.com>
2020-03-12 16:58:05 +0100

    yikes

--- mtgdraft/client.py
+++ mtgdraft/client.py
@@ -1,29 +1,79 @@
+from __future__ import annotations
+
+import typing as t
 import json
 import threading
-import typing as t
+
 from abc import ABC, abstractmethod
 
 import websocket
 
-from cubeclient.models import User, ApiClient
-from magiccube.laps.purples.purple import Purple
-from magiccube.laps.tickets.ticket import Ticket
-from magiccube.laps.traps.trap import Trap
 from mtgorp.db.database import CardDatabase
 from mtgorp.models.persistent.printing import Printing
 from mtgorp.models.serilization.strategies.raw import RawStrategy
 
+from magiccube.laps.purples.purple import Purple
+from magiccube.laps.tickets.ticket import Ticket
+from magiccube.laps.traps.trap import Trap
 from magiccube.collections import cubeable as Cubeable
+
+from cubeclient.models import User, ApiClient
 
 from mtgdraft.models import Booster, DraftRound
 
+_deserialize_type_map = {
+    'Trap': Trap,
+    'Ticket': Ticket,
+    'Purple': Purple,
+}
+
+
+def _serialize_cubeable(cubeable: Cubeable) -> t.Any:
+    return cubeable.id if isinstance(cubeable, Printing) else RawStrategy.serialize(cubeable)
+
+
+class DraftFormat(ABC):
+
+    def __init__(self, draft_client: DraftClient):
+        self._draft_client = draft_client
+
+    def pick(self, *args, **kwargs) -> t.Any:
+        self._draft_client.socket.send(
+            json.dumps(
+                {
+                    'type': 'pick',
+                    'pick': self._pick(*args, **kwargs),
+                }
+            )
+        )
+
+    @abstractmethod
+    def _pick(self, *args, **kwargs) -> t.Any:
+        pass
+
+
+class SinglePick(DraftFormat):
+
+    def _pick(self, pick: Cubeable) -> t.Any:
+        return _serialize_cubeable(pick)
+
+
+class Burn(DraftFormat):
+
+    def _pick(self, pick: Cubeable, burn: t.Optional[Cubeable]) -> t.Any:
+        return {
+            'pick': _serialize_cubeable(pick),
+            'burn': _serialize_cubeable(burn) if burn is not None else None,
+        }
+
+
+draft_format_map = {
+    'single_pick': SinglePick,
+    'burn': Burn,
+}
+
 
 class DraftClient(ABC):
-    _deserialize_type_map = {
-        'Trap': Trap,
-        'Ticket': Ticket,
-        'Purple': Purple,
-    }
 
     def __init__(self, api_client: ApiClient, draft_id: str, db: CardDatabase):
         self._api_client = api_client
@@ -31,7 +81,7 @@ class DraftClient(ABC):
         self._db = db
 
         self._drafters: t.Optional[t.List[User]] = None
-        self._draft_format: t.Optional[str] = None
+        self._draft_format: t.Optional[DraftFormat] = None
 
         self._pool_id: t.Optional[int] = None
         self._session_name: t.Optional[str] = None
@@ -57,12 +107,20 @@ class DraftClient(ABC):
         self._ws_thread = threading.Thread(target = self._ws.run_forever, daemon = True)
         self._ws_thread.start()
 
+    @property
+    def socket(self) -> websocket.WebSocketApp:
+        return self._ws
+
     def close(self):
         self._ws.close()
 
     @property
     def drafters(self) -> t.List[User]:
         return self._drafters
+
+    @property
+    def draft_format(self) -> DraftFormat:
+        return self._draft_format
 
     @property
     def round(self) -> DraftRound:
@@ -81,7 +139,7 @@ class DraftClient(ABC):
             self._db.printings[cubeable]
             if isinstance(cubeable, int) else
             RawStrategy(self._db).deserialize(
-                self._deserialize_type_map[cubeable['type']],
+                _deserialize_type_map[cubeable['type']],
                 cubeable,
             )
         )
@@ -91,7 +149,7 @@ class DraftClient(ABC):
         pass
 
     @abstractmethod
-    def _picked(self, pick: Cubeable, pick_number: int) -> None:
+    def _picked(self, pick: t.Any, pick_number: int) -> None:
         pass
 
     @abstractmethod
@@ -105,16 +163,6 @@ class DraftClient(ABC):
     @abstractmethod
     def _on_round(self, draft_round: DraftRound) -> None:
         pass
-
-    def pick(self, cubeable: Cubeable) -> None:
-        self._ws.send(
-            json.dumps(
-                {
-                    'type': 'pick',
-                    'pick': cubeable.id if isinstance(cubeable, Printing) else RawStrategy.serialize(cubeable),
-                }
-            )
-        )
 
     def on_error(self, error):
         print(error)
@@ -143,7 +191,18 @@ class DraftClient(ABC):
 
         elif message_type == 'pick':
             self._picked(
-                self._deserialize_cubeable(message['pick']),
+                (
+                    self._deserialize_cubeable(message['pick'])
+                    if isinstance(self._draft_format, SinglePick) else
+                    {
+                        'pick': self._deserialize_cubeable(message['pick']['pick']),
+                        'burn': (
+                            self._deserialize_cubeable(message['pick']['burn'])
+                            if message['pick']['burn'] is not None else
+                            None
+                        ),
+                    }
+                ),
                 message['pick_number'],
             )
 
@@ -159,6 +218,7 @@ class DraftClient(ABC):
                 ) for user in
                 message['drafters']
             ]
+            self._draft_format = draft_format_map[message['draft_format']](self)
             self._on_start()
 
         elif message_type == 'completed':
